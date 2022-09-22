15d11b45fc38c2e1f59974d49642841ed92de644
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-09-22 21:18:07 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryList(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+galleryContainer.addEventListener('click', onImgClick);
+
+function createGalleryList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+       return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+}
+
+function onImgClick(event) {
+    event.preventDefault();
+   
+    if (!event.target.classList.contains('gallery__image')){
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+}
+    
+
